a1550e4ad2fc36dbfdd3fdb26a3eade144473a4d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-10 22:49:45 +0300

    fix linters

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-class App
-  def initialize 
-     #will put arrays later
+class Main
+  def initialize
+    # will put arrays later
   end
-  
+
   def menu
     puts "\n"
     puts 'please choose an option by entering a number:
@@ -13,54 +13,53 @@ class App
     5 - List all lables
     6 - List all authors
     7 - Add a book
-    8 - Add a music album 
+    8 - Add a music album
     9 - Add a game
     10 - Exit'
- end
-
- def handle_selection
-   user_input  = gets.chomp 
-   case user_input
-     when '1'
-       puts 'all Books'
-     when '2'
-       puts 'all music albums'
-     when '3'
-       puts 'all games'
-     when '4'
-       puts 'all genres'
-     when '5'
-       puts 'all lables'
-     when '6'
-       puts 'all authors'
-     when '7'
-       puts 'Add a book'
-     when '8'
-       puts 'Add a music album'
-     when '9'
-       puts 'Add a game'
-     when '10'
-       puts 'Thanks for using the app'
-       exit 
-     else 
-      puts 'Wrong input'   
-    end
-    
-end
- 
-def run 
-  user_input = nil
-  puts "Welcome to the catalog of my things"
-
-  while user_input != "10"
-    menu
-    handle_selection 
   end
-end  
-end  
+
+  def handle_selection
+    user_input = gets.chomp
+    case user_input
+    when '1'
+      puts 'all Books'
+    when '2'
+      puts 'all music albums'
+    when '3'
+      puts 'all games'
+    when '4'
+      puts 'all genres'
+    when '5'
+      puts 'all lables'
+    when '6'
+      puts 'all authors'
+    when '7'
+      puts 'Add a book'
+    when '8'
+      puts 'Add a music album'
+    when '9'
+      puts 'Add a game'
+    when '10'
+      puts 'Thanks for using the app'
+      exit
+    else
+      puts 'Wrong input'
+    end
+  end
+
+  def run
+    user_input = nil
+    puts 'Welcome to the catalog of my things'
+
+    while user_input != '10'
+      menu
+      handle_selection
+    end
+  end
+end
 
 def main
-  app = App.new
+  app = Main.new
   app.run
 end
 
