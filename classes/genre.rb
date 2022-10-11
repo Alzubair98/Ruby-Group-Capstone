2a6c8533bf481907932bb8e791258f4934c9b6f4
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-11 09:34:13 +0430

    Add attributes and methods of genre and music_album

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,3 +1,14 @@
 class Genre
- 
+ attr_reader :items, :name
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.genre = self
+  end
 end
--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,3 +1,13 @@
+require_relative './item'
+attr_accessor :on_spotify
+
 class MusicAlbum
-  
+  def initialize(on_spotify, publish_date = Time.now)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?()
+    super() and @on_spotify
+  end
 end
