983bacef78770317555e37ea3abf8632953fb407
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-11 09:39:29 +0430

    Add tests to genre

---
+++ spec/genre_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../classes/genre'
+require_relative '../classes/item'
+
+describe Genre do
+  before(:each) do
+    @genre = Genre.new('comedy')
+    @item = Item.new(@genre)
+  end
+
+  it 'has name to be comedy' do
+    expect(@genre.name).to eq 'comedy'
+  end
+
+  it 'has 1 genre' do
+    @genre.add_item(@item)
+    expect(@genre.items.size).to be 1
+  end
+end
