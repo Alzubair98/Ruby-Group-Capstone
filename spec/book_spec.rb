26c99c9c19bfa1249056b43c562bfa58cdca96fd
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-11 11:28:56 +0300

    fix the tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,13 +2,15 @@ require_relative '../classes/book'
 
 describe Book do
   context 'create new book object from book class' do
-    book = Book.new('publisher1', 'new')
+    book = Book.new('publisher1', 'new', 'good')
 
     it 'Create book' do
       publisher = book.publisher
       cover = book.cover_state
+      label = book.label
       expect(publisher).to eq('publisher1')
       expect(cover).to eq('new')
+      expect(label).to eq('good')
     end
 
     it 'check can_be_archived method' do
