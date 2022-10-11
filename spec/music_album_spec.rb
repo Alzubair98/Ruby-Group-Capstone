22d2809047ca5f5a8ce53bd1455834c52756588b
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-11 09:42:26 +0430

    Add tests to music album

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../classes/music_album'
+
+describe MusicAlbum do
+  before(:each) do
+    @musicalbum = MusicAlbum.new(true, 'Favorate', '02/15/1999')
+  end
+
+  it 'can has an instance' do
+    expect(@musicalbum).to be_an_instance_of MusicAlbum
+  end
+
+  it 'has on_spotify to be true' do
+    expect(@musicalbum.on_spotify).to be true
+  end
+
+  it 'has name to be Favorate' do
+    expect(@musicalbum.name).to eq 'Favorate'
+  end
+
+  it 'has publish_date to be 02/15/1999' do
+    expect(@musicalbum.publish_date).to eq '02/15/1999'
+  end
+end
