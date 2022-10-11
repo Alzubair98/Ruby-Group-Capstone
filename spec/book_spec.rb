ec360a6ea3a9b1caee21c204c8a9d6bdf5599508
Sayed Faqir Bares <sayedfaqirbares@Sadat-MacBook-Pro.local>
2022-10-11 20:03:39 +0430

    Update

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,21 +1,30 @@
-require_relative '../classes/book'
+require 'date'
+require './classes/book'
 
 describe Book do
-  context 'create new book object from book class' do
-    book = Book.new('publisher1', 'new', 'good')
-
-    it 'Create book' do
-      publisher = book.publisher
-      cover = book.cover_state
-      label = book.label
-      expect(publisher).to eq('publisher1')
-      expect(cover).to eq('new')
-      expect(label).to eq('good')
+  context 'Book class tests' do
+    before(:each) do
+      @book = Book.new('Zubair', Date.parse('2020/10/10'), 'good')
     end
 
-    it 'check can_be_archived method' do
-      archive = book.can_be_archived?
-      expect(archive).to be false
+    it 'should be a Book class instance' do
+      expect(@book).to be_an_instance_of Book
+    end
+
+    it 'should return correct book publisher' do
+      expect(@book.publisher).to eq 'Zubair'
+    end
+
+    it 'should return correct book cover state' do
+      expect(@book.cover_state).to eq 'good'
+    end
+
+    it 'should return correct book publish_date' do
+      expect(@book.publish_date).to eq Date.parse('2020/10/10')
+    end
+
+    it 'should return false unless @archived || cover_state == bad' do
+      expect(@book.can_be_archived?).to eq false
     end
   end
 end
