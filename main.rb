a03c7c121ea08e18faccbfb123e8be028df4349b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-10-11 17:30:36 +0300

    fix typo

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ class Main
     2 - List all music albums
     3 - List all games
     4 - List all genres
-    5 - List all lables
+    5 - List all labels
     6 - List all authors
     7 - Add a book
     8 - Add a music album
